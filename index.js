0a00d16c849e0e9828f665f507c6a9ad01c439f6
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 15:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  animal = 'cat';
-  return animal
+  animal1 = 'cat';
+  return animal1
 }
 
 function add2(n) {
